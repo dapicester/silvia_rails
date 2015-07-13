9d3cf06362ae8405519964f047531d96239ab6ac
Paolo D'Apice <paolo@hashtag.be>
2015-07-13 10:32:58 +0800

    ApplicationHelper#full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'Il mondo di Silvia'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
