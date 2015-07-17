60c5a2d1bd364ea51d8351706201ca41d9532f6a
Paolo D'Apice <paolo@hashtag.be>
2015-07-17 17:37:46 +0800

    Admin can delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 User.create! name: 'Amministratore',
              email: 'admin@ilmondodisilvia.org',
              password: 'admin123',
-             password_confirmation: 'admin123'
+             password_confirmation: 'admin123',
+             admin: true
 
 10.times do |i|
   User.create! name: Faker::Name.name,
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,15 +2,30 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users :paolo
+    @admin = users :paolo
+    @non_admin = users :silvia
   end
-  test 'index including pagination' do
-    log_in_as @user
+
+  test 'index as admin including pagination and delete link' do
+    log_in_as @admin
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    User.paginate(page: 1).each do |user|
+    first_page = User.paginate page: 1
+    first_page.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: t('users.index.delete')
+      end
     end
+    assert_difference 'User.count', -1 do
+      delete user_path @non_admin
+    end
+  end
+
+  test 'index as non-admin' do
+    log_in_as @non_admin
+    get users_path
+    assert_select 'a', text: t('users.index.delete'), count: 0
   end
 end
