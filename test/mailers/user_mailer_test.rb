e2b7a4160c53a92c2dd77442ad34c0957aed89f8
Paolo D'Apice <paolo@hashtag.be>
2015-07-20 18:09:53 +0800

    Password reset

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -13,4 +13,14 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI::escape(user.email), mail.body.encoded
   end
 
+  test 'password_reset' do
+    user = users :paolo
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset user
+    assert_equal t('user_mailer.password_reset.subject'), mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal %w[no-reply@example.com], mail.from
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI::escape(user.email), mail.body.encoded
+  end
 end
